aa202ceb15829b10bda8843554cc67b91e974294
Kevin van Ingen <kin@synadia.nl>
2021-03-18 22:19:39 +0100

    Added aws-cli-js library and created example script.

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# aws-iot-rule-script
+
+# AWS IOT Rules insert script
+
+This script uses the [https://www.npmjs.com/package/aws-cli-js] library.  It works nicely with exported AWS sessions from your environment variables, like when using [https://github.com/99designs/aws-vault].  
+
+## Environment vars
+The script uses five environment params as strings.
+
+Setting environment variables can be performed like this:
+
+```
+export TIMESTREAM_INSERT_ROLE=
+export TIMESTREAM_ERROR_LOG_ROLE=
+export TIMESTREAM_DB_NAME=
+export TIMESTREAM_TABLE_NAME=
+export TIMESTREAM_ERROR_LOG_GROUP=
+```
+
+
---
+++ script.ts
@@ -0,0 +1,70 @@
+import {Aws, Options} from 'aws-cli-js';
+
+const options = new Options(
+    process.env.AWS_ACCESS_KEY_ID,
+    process.env.AWS_SECRET_ACCESS_KEY,
+    process.env.AWS_SESSION_TOKEN
+);
+
+function validateVars() {
+    if(!process.env.AWS_SESSION_TOKEN) {
+        throw Error('This script needs a valid AWS Session.');
+    }else if(!process.env.TIMESTREAM_INSERT_ROLE) {
+        throw Error('This script needs a role to allow it to insert into TimeStream.');
+    }else if(!process.env.TIMESTREAM_ERROR_LOG_ROLE) {
+        throw Error('This script needs a role to allow it to insert into TimeStream.');
+    }else if(!process.env.TIMESTREAM_ERROR_LOG_GROUP) {
+        throw Error('This script needs a error log group write error logs to Cloudwatch.');
+    }else if(!process.env.TIMESTREAM_DB_NAME) {
+        throw Error('This script needs a Timestream database name.');
+    }else if(!process.env.TIMESTREAM_TABLE_NAME) {
+        throw Error('This script needs a Timestream table name.');
+    }
+}
+
+const runUpdate = async () => {
+
+    validateVars();
+    await insertTimeStreamRule('test-rule');
+}
+
+const aws = new Aws(options);
+console.log('Update IOT Rules');
+runUpdate().then(value =>console.log('Finished importing IOT rules.')).catch(reason => console.error(`Problem importing IOT rules ${JSON.stringify(reason)}`));
+
+
+async function insertTimeStreamRule(ruleName: string): Promise<any> {
+    let dataReturn;
+    const payload = `{ \
+        \\\"sql\\\": \\\"SELECT value FROM '\\\$aws/sitewise/#'\\\", \
+        \\\"awsIotSqlVersion\\\": \\\"2016-03-23\\\", \
+        \\\"description\\\":\\\"${ruleName}\\\",\
+        \\\"actions\\\":[\
+            {\
+                \\\"timestream\\\":{\
+                    \\\"roleArn\\\":\\\"${process.env.TIMESTREAM_INSERT_ROLE}\\\",\
+                    \\\"databaseName\\\":\\\"${process.env.TIMESTREAM_DB_NAME}\\\",\
+                    \\\"tableName\\\":\\\"${process.env.TIMESTREAM_TABLE_NAME}\\\",\
+                    \\\"dimensions\\\":[ \
+                        {\\\"name\\\":\\\"property\\\",\\\"value\\\":\\\"dimensional-value\\\"}, \
+                    ]\
+                }\
+            }\
+        ], \
+         \\\"errorAction\\\":{\
+             \\\"cloudwatchLogs\\\":{ \
+                  \\\"logGroupName\\\":\\\"${process.env.TIMESTREAM_ERROR_LOG_GROUP}\\\", \
+                  \\\"roleArn\\\":\\\"${process.env.TIMESTREAM_ERROR_LOG_ROLE}\\\" \
+              }\ 
+           }\
+    }`;
+
+    await aws.command("iot create-topic-rule --rule-name " + ruleName + " --topic-rule-payload \"" + payload + "\"").then(function (data: any) {
+        console.log(`Successful added rule for asset ${ruleName}`);
+        dataReturn = data;
+    }).catch(function (error: any) {
+        console.error(error);
+        throw Error(error);
+    });
+    return dataReturn;
+}
